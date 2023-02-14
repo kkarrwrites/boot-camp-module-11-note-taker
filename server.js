2ea0d4c1ed4a0eac508f7105782a0fc5d7e3baca
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-14 14:05:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // TODOs
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 // When I click on an existing note in the list in the left-hand column, then that note appears in the right-hand column
 // When I click on the Write icon in the navigation at the top of the page, then I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-// /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
 const fs = require("fs");
 const path = require("path");
