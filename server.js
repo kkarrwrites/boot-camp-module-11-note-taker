fbba0581dd87d4a1672dd9ad374fd84dc22ebcbc
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-16 11:05:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js");
 
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
