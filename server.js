4641ffacbf22d2b28b30680b23a59f39844c1357
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-16 09:50:50 -0500

    Add period before /public/index.html and /public/notes.html

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(express.static("public"));
 
 // Route for static index page
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 // Route for static notes page
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 // Route for showing notes
